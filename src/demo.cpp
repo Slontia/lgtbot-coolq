1de60eb632fcab1d65d9c6e57c61e4f1e85bda1a
slontia <654867229@qq.com>
2020-02-09 21:50:55 +0800

    link BotCore

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,66 +1,137 @@
 #include <iostream>
 #include <set>
 #include <sstream>
+#include <fstream>
+#include <functional>
 
 #include <cqcppsdk/cqcppsdk.h>
 
+#include "../lib/dllmain.h"
+
+#include <windows.h>
+
 using namespace cq;
-using namespace std;
 using Message = cq::message::Message;
 using MessageSegment = cq::message::MessageSegment;
 
+static inline std::pair<bool, int64_t> to_group_or_discuss_id(const GroupID gid)
+{
+    const bool is_group = gid & (uint64_t(1) << 63);
+    return { is_group, gid & ~(uint64_t(1) << 63) };
+}
+
+static inline uint64_t to_gid(const int64_t id, const bool is_group)
+{
+    return is_group ? (id | (uint64_t(1) << 63)) : id;
+}
+
+std::string gbk_to_utf8(const char* strGBK)
+{
+    int len = MultiByteToWideChar(CP_ACP, 0, strGBK, -1, NULL, 0);
+    wchar_t* wstr = new wchar_t[len + 1];
+    memset(wstr, 0, len + 1);
+    MultiByteToWideChar(CP_ACP, 0, strGBK, -1, wstr, len);
+    len = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, NULL, 0, NULL, NULL);
+    char* str = new char[len + 1];
+    memset(str, 0, len + 1);
+    WideCharToMultiByte(CP_UTF8, 0, wstr, -1, str, len, NULL, NULL);
+    std::string strTemp = str;
+    if(wstr) delete[] wstr;
+    if(str) delete[] str;
+    return strTemp;
+}
+
+std::string utf8_to_gbk(const char* strUTF8)
+{
+    int len = MultiByteToWideChar(CP_UTF8, 0, strUTF8, -1, NULL, 0);
+    wchar_t* wszGBK = new wchar_t[len + 1];
+    memset(wszGBK, 0, len * 2 + 2);
+    MultiByteToWideChar(CP_UTF8, 0, strUTF8, -1, wszGBK, len);
+    len = WideCharToMultiByte(CP_ACP, 0, wszGBK, -1, NULL, 0, NULL, NULL);
+    char* szGBK = new char[len + 1];
+    memset(szGBK, 0, len + 1);
+    WideCharToMultiByte(CP_ACP, 0, wszGBK, -1, szGBK, len, NULL, NULL);
+    std::string strTemp(szGBK);
+    if(wszGBK) delete[] wszGBK;
+    if(szGBK) delete[] szGBK;
+    return strTemp;
+}
+
+std::pair<bool, std::string> is_at_me(std::string msg)
+{
+    const std::string at_me_str = MessageSegment::at(get_login_user_id());
+    bool is_at_me = false;
+    for (size_t i = msg.find(at_me_str); i != std::string::npos; i = msg.find(at_me_str))
+    {
+        is_at_me = true;
+        msg.replace(i, i + at_me_str.size(), " ");
+    }
+    return { is_at_me, msg };
+}
+
+void send_message_spilt_pages(const std::function<void(const std::string&)>& send, const std::string& msg, const uint64_t lines_each_page = 16)
+{
+    std::stringstream iss(msg);
+    for (std::string line; std::getline(iss, line); )
+    {
+        std::stringstream oss;
+        oss << line;
+        for (uint64_t i = 1; i < lines_each_page && std::getline(iss, line); ++ i)
+        {
+            oss << std::endl << line;
+        }
+        send(oss.str());
+    }
+}
+
+void lgt_send_private_msg(const UserID& uid, const char* gbk_msg)
+{
+    std::string msg = gbk_to_utf8(gbk_msg);
+    try
+    {
+        send_message_spilt_pages(std::bind(send_private_message, uid, std::placeholders::_1), msg);
+    }
+    catch (ApiError &e) {}
+}
+
+void lgt_send_public_msg(const GroupID& gid, const char* gbk_msg)
+{
+    std::string msg = gbk_to_utf8(gbk_msg);
+    const auto [is_group, id] = to_group_or_discuss_id(gid);
+    try
+    {
+        send_message_spilt_pages(std::bind(is_group ? send_group_message : send_discuss_message, id, std::placeholders::_1), msg);
+    }
+    catch (ApiError &e) {}
+}
+
+void lgt_at(const UserID& uid, char* buf, const uint64_t len)
+{
+    std::string at_str = MessageSegment::at(uid);
+    strcpy_s(buf, len, at_str.c_str());
+}
+
 CQ_INIT {
-    on_enable([] { logging::info("启用", "插件已启用"); });
-
-    on_private_message([](const PrivateMessageEvent &e) {
-        try {
-            auto msgid = send_private_message(e.user_id, e.message); // 直接复读消息
-            logging::info_success("私聊", "私聊消息复读完成, 消息 Id: " + to_string(msgid));
-            send_message(e.target,
-                         MessageSegment::face(111) + "这是通过 message 模块构造的消息~"); // 使用 message 模块构造消息
-        } catch (ApiError &e) {
-            logging::warning("私聊", "私聊消息复读失败, 错误码: " + to_string(e.code));
-        }
+    on_enable([]
+    {
+        BOT_API::Init(get_login_user_id(), lgt_send_private_msg, lgt_send_public_msg, lgt_at);
     });
 
-    on_message([](const MessageEvent &e) {
-        logging::debug("消息", "收到消息: " + e.message + "\n实际类型: " + typeid(e).name());
+    on_private_message([](const PrivateMessageEvent &e)
+    {
+        BOT_API::HandlePrivateRequest(e.user_id, utf8_to_gbk(e.message.c_str()).c_str());
     });
 
-    on_group_message([](const GroupMessageEvent &e) {
-        static const set<int64_t> ENABLED_GROUPS = {123456, 123457};
-        if (ENABLED_GROUPS.count(e.group_id) == 0) return; // 不在启用的群中, 忽略
-
-        try {
-            send_message(e.target, e.message); // 复读
-            auto mem_list = get_group_member_list(e.group_id); // 获取群成员列表
-            string msg;
-            for (auto i = 0; i < min(10, static_cast<int>(mem_list.size())); i++) {
-                msg += "昵称: " + mem_list[i].nickname + "\n"; // 拼接前十个成员的昵称
-            }
-            send_group_message(e.group_id, msg); // 发送群消息
-        } catch (ApiError &) { // 忽略发送失败
-        }
-        if (e.is_anonymous()) {
-            logging::info("群聊", "消息是匿名消息, 匿名昵称: " + e.anonymous.name);
-        }
-        e.block(); // 阻止当前事件传递到下一个插件
+    on_group_message([](const GroupMessageEvent &e)
+    {
+        const auto& [ok, msg] = is_at_me(utf8_to_gbk(e.message.c_str()));
+        if (ok) { BOT_API::HandlePublicRequest(e.user_id, to_gid(e.group_id, true), msg.c_str()); }
+        
     });
 
-    on_group_upload([](const auto &e) { // 可以使用 auto 自动推断类型
-        stringstream ss;
-        ss << "您上传了一个文件, 文件名: " << e.file.name << ", 大小(字节): " << e.file.size;
-        try {
-            send_message(e.target, ss.str());
-        } catch (ApiError &) {
-        }
+    on_discuss_message([](const DiscussMessageEvent &e)
+    {
+        const auto& [ok, msg] = is_at_me(utf8_to_gbk(e.message.c_str()));
+        if (ok) { BOT_API::HandlePublicRequest(e.user_id, to_gid(e.discuss_id, true), msg.c_str()); }
     });
-}
-
-CQ_MENU(menu_demo_1) {
-    logging::info("菜单", "点击菜单1");
-}
-
-CQ_MENU(menu_demo_2) {
-    send_private_message(10000, "测试");
 }
